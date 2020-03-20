956b7ca3969601edd4993b4d9702ac121b9e3639
wkibbbler <william.kibbler@googlemail.com>
2020-03-20 23:23:15 +0000

    add dash back

--- plusbit.js
+++ plusbit.js
@@ -146,10 +146,10 @@ function getZel(params, cb){
 }
 }
 
-function getZcash(params, cb){
+function getDash(params, cb){
   let heightList = new Array
   try {
-  request(`https://explorer.z.cash/api/addr/${params.address}`, { json: true }, (err, res, balances) => {
+  request(`https://insight.dash.org//api/addr/${params.address}`, { json: true }, (err, res, balances) => {
     if (typeof balances !== 'object'){
       cb({
         price: '0.00',
@@ -161,13 +161,14 @@ function getZcash(params, cb){
         heightList: []
       })
     } else {
-      request(`https://explorer.z.cash/api/txs/?address=${params.address}`, { json: true }, (err, res, transactions) => {
+      request(`https://insight.dash.org//api/txs/?address=${params.address}`, { json: true }, (err, res, transactions) => {
       let formatedTransactions = new Array
       transactions.txs.forEach(tx => {
         heightList.push(65)
         formatedTransactions.push({
           txid: tx.txid,
-          direction: tx.vin.length == 0 ? 'RECEIVED' : tx.vin[0].addr == params.address ? 'SENT' : 'RECEIVED',
+          direction: tx.vin[0].addr == params.address ? 'SENT' : 'RECEIVED',
+          to_from: tx.vin[0].addr == params.address ? tx.vout[0].scriptPubKey.addresses[0] : tx.vin[0].addr,
           value: Number(tx.vout[0].value),
           fiatValue: parseFloat((tx.vout[0].value * params.price).toFixed(2)),
           date: moment(tx.time * 1000).format("DD/MM/YYYY"),
@@ -187,25 +188,25 @@ function getZcash(params, cb){
     }
   })
 } catch (err) {
-  throw new Error('Error with zcash, request will timeout ', err)
+  throw new Error('Error with dash, request will timeout ', err)
 }
 }
 
-app.get("/plusbit/:fiatUnit/:bitcoin/:ilcoin/:zel/:zcash", (req, res, next) => {
+app.get("/plusbit/:fiatUnit/:bitcoin/:ilcoin/:zel/:dash", (req, res, next) => {
   var response = res
-  request(`https://api.coingecko.com/api/v3/simple/price?ids=ilcoin,bitcoin,zelcash,zcash&vs_currencies=try,usd,eur,chf,cad,aud,gbp,jpy,nzd,cny,zar,thb,php,krw,vnd,myr,rub,inr,sgd,hkd,ars,brl,dkk,idr,kwd,mxn,nok,pln,pkr,sar,sek,uah`, { json: true }, (err, res, body) => {
+  request(`https://api.coingecko.com/api/v3/simple/price?ids=ilcoin,bitcoin,zelcash,dash&vs_currencies=try,usd,eur,chf,cad,aud,gbp,jpy,nzd,cny,zar,thb,php,krw,vnd,myr,rub,inr,sgd,hkd,ars,brl,dkk,idr,kwd,mxn,nok,pln,pkr,sar,sek,uah`, { json: true }, (err, res, body) => {
     let fiatUnit = req.params.fiatUnit.toLowerCase()
 
     getBitcoin({address: req.params.bitcoin, price: body.bitcoin[fiatUnit], unit: req.params.fiatUnit}, function(bitcoin_balances){
       getIlcoin({address: req.params.ilcoin, price: body.ilcoin[fiatUnit], unit: req.params.fiatUnit}, function(ilcoin_balances){
         getZel({address: req.params.zel, price: body.zelcash[fiatUnit], unit: req.params.fiatUnit}, function(zel_balances){
-          getZcash({address: req.params.zcash, price: body.zcash[fiatUnit], unit: req.params.fiatUnit}, function(zcash_balances){
+          getDash({address: req.params.dash, price: body.dash[fiatUnit], unit: req.params.fiatUnit}, function(dash_balances){
             response.json({
-              totalBalance: currencyFormatter.format(bitcoin_balances.rawFiat + ilcoin_balances.rawFiat + zel_balances.rawFiat + zcash_balances.rawFiat, { code: req.params.fiatUnit }),
+              totalBalance: currencyFormatter.format(bitcoin_balances.rawFiat + ilcoin_balances.rawFiat + zel_balances.rawFiat + dash_balances.rawFiat, { code: req.params.fiatUnit }),
               BTC: bitcoin_balances,
               ILC: ilcoin_balances,
               ZEL: zel_balances,
-              ZEC: zcash_balances
+              DASH: dash_balances
             })
           })
         })
